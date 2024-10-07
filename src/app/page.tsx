ec7eec654e100fbe9bccdfb71f33e91012248c9a
JucierG <jucier.gregorio05@gmail.com>
2024-10-07 16:50:35 -0300

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { Title } from "./components/title";
+
 
 export default function Home() {
   return (
     <div>
-      <h1>Jucier Gregório</h1>
+      <h1>Página Home</h1>
+      <Title />
     </div>
   );
 }
